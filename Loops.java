cd8e82bc95c84775d439cbaa9b05c4c45691d6da
Elaine Ottero <elainegui@github.com>
2020-11-11 07:59:21 +0000

    loops and webinar questions

---
+++ Loops.java
@@ -0,0 +1,14 @@
+package eCollegeJava;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		/*	int count =1;
+			for (count<5){
+			System.out.println();
+			count++;
+			};*/
+
+	}
+
+}
--- Webinars.java
+++ Webinars.java
@@ -11,20 +11,67 @@ public class Webinars {
 
 		System.out.println(x + " " + y);
 
+		/*
+		Q35: What is the value of x and y (if any)?
+
+		A: 10 10
+		B: 0 10
+		C: 1000 10
+		D: 10 1000
+		E: The program fails to compile
+		F: None of these answers are correct
+
+		*/
+
+		/*Answer: That code will compile. The *= indicates that a cast is putting automatically by the compiler.
+		Don't worry about the loss of precision
+		*/
+
+		float changeInPocket = 1.45f;
+		float priceOfTicket = 5.00f;
+		String buyTicket = changeInPocket >= priceOfTicket ? "Admittance granted!" : "Admittance denied!";
+		System.out.println(buyTicket);
+
+		/*
+		Q44: What is the output of the program (if any?).
+		
+		A: Admittance granted!
+
+		B: Admittance denied!
+
+		C: The program fails to compile (there is no output).
+
+		D: The program compiles but crashes due to a runtime error.
+
+		*/
+
+		String[] arguments = { "X", "Y", "Z" };
+
+		int x = 0;
+		int y = 3;
+		do {
+			System.out.println(arguments[++x]);
+		} while (x++ < --y);
+
+		/*
+		Q65: What is the output of the program (if any?)
+		
+		A: X
+		   Y
+		   Z
+
+		B: Z
+		   Y
+		   X
+
+		C: Y (followed by an ArrayIndexOutOfBoundsException)
+		
+		D: Z (followed by an ArrayIndexOutOfBoundsException)
+		
+		E: X (followed by an ArrayIndexOutOfBoundsException)
+		
+		F: The program fails to compile (there is no output).
+
+		*/
 	}
-	/*
-	Q35: What is the value of x and y (if any)?
-	
-	A: 10 10
-	B: 0 10
-	C: 1000 10
-	D: 10 1000
-	E: The program fails to compile
-	F: None of these answers are correct
-
-	*/
-
-	/*Answer: That code will compile. The *= indicates that a cast is putting automatically by the compiler.
-	Don't worry about the loss of precision
-	*/
 }
