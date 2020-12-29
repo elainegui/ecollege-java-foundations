3e0a6deb94b8add15e28bfeba941b2cbd6a9c18c
Elaine Ottero <elainegui@github.com>
2020-12-29 11:17:21 +0000

    string

--- Strings.java
+++ Strings.java
@@ -7,6 +7,24 @@ public class Strings {
 		int index = str.lastIndexOf('s', 5);
 		System.out.println(index);
 
+		String javaFoundations = new String("Java Foundations");
+		String jFoundations = "Java Foundations";
+		System.out.println(javaFoundations == jFoundations);
+		System.out.println(javaFoundations.equals(jFoundations));
+		System.out.println(javaFoundations.compareTo(jFoundations));
+
+		String s1 = new String("dinghy");
+		String s2 = "dinghy";
+		String s3 = s2;
+		s1 = s1.concat("rubber");
+		s2.concat(s1);
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+		System.out.println(s2 == s3);
+		System.out.println(s2.equals(s3));
+		System.out.println(s1 == s3);
+		System.out.println(s1.equals(s3));
+		System.out.println(s1 + " " + s2);
 	}
 
 }
