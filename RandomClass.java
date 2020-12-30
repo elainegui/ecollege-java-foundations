33fbf4ad7b8372414e2bf6fdb8be015ac9fbc6c3
Elaine Ottero <elainegui@github.com>
2020-12-30 15:30:49 +0000

    Random

---
+++ RandomClass.java
@@ -0,0 +1,30 @@
+package eCollegeJava;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomClass {
+
+	public static void main(String[] args) {
+		// Random class
+		//stored in the java.util package
+		
+		Random r = new Random();
+		int randomNo = r.nextInt(6);
+		//the nextInt(int bound) generates a random number between 0 (inclusive) and the specified value (exclusive)
+		System.out.println(randomNo);
+		randomNo = r.nextInt();
+		//nextInt()
+		//All 232 possible int values are produced with (approximately) equal probability.
+		System.out.println(randomNo);
+
+		//nextBytes(byteArray)
+		//generates random bytes and places them into a user-supplied byte array
+		byte[] byteArray = new byte[3];
+		byteArray[0] = 1;
+		System.out.println(Arrays.toString(byteArray));
+		r.nextBytes(byteArray);
+		System.out.println(Arrays.toString(byteArray));
+	}
+
+}
